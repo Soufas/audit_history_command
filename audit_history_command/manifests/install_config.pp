5bc638f597726f9c6000f87dd5c792ce78d80a75
seif karoui <karouiseif@gmail.com>
2017-05-26 19:26:28 +0100

    fxied style

--- audit_history_command/manifests/install_config.pp
+++ audit_history_command/manifests/install_config.pp
@@ -1,3 +1,12 @@
+# Class: install_config
+# Parameters: none
+#
+# Actions:
+#
+# Requires: None
+#
+# Simple class to install and confgure
+#
 class audit_history_command::install_config
 {
 file {'/usr/local/bin/hcmnt':
@@ -7,22 +16,22 @@ file {'/usr/local/bin/hcmnt':
   owner  => 'root',
 }
 file {'/usr/local/bin/caf':
-  ensure => present,
-  source => 'puppet:///modules/audit_history_command/caf',
-  mode   => '755',
-  owner  => 'root',
+  ensure    => present,
+  source    => 'puppet:///modules/audit_history_command/caf',
+  mode      => '755',
+  owner     => 'root',
   subscribe => File['/usr/local/bin/hcmnt']
 }
 file {'/tmp/bashrc_lines':
-  ensure => present,
-  source => 'puppet:///modules/audit_history_command/bashrc_lines',
-  mode   => '755',
-  owner  => 'root',
+  ensure    => present,
+  source    => 'puppet:///modules/audit_history_command/bashrc_lines',
+  mode      => '755',
+  owner     => 'root',
   subscribe => File['/usr/local/bin/hcmnt']
 }
 exec {'edit_bashrc':
-  command => "/usr/bin/cat /tmp/bashrc_lines >> /etc/bashrc",
-  subscribe => File['/tmp/bashrc_lines'],
+  command     => '/usr/bin/cat /tmp/bashrc_lines >> /etc/bashrc',
+  subscribe   => File['/tmp/bashrc_lines'],
   refreshonly => true,
 }
 
